0862fab45bc9d90a5cb42476578d657512d61e57
Jillur <jillur@Mds-Mac-mini.local>
2025-06-04 15:13:12 +0600

    dashabordss to dashboards

--- src/layout/dashboard/Dashboard.tsx
+++ src/layout/dashboard/Dashboard.tsx
@@ -46,7 +46,7 @@ interface MenuItem {
 const menuItems: MenuItem[] = [
   {
     path: "/",
-    title: "Dashboardss",
+    title: "Dashboards",
     icon: <BiPieChartAlt2 size={18} color="white" />,
     activeIcon: <FaChartPie size={18} color="white" />,
   },
