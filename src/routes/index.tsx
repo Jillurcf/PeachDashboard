fae3bb8f8b38c2acbedb87dab3fb9bd356a7e144
Jillur <jillur@Mds-Mac-mini.local>
2025-06-20 15:25:43 +0600

    Adminroute added to the dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -45,7 +45,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element:<DasboardHome />
+                element:<AdminRoutes><DasboardHome /></AdminRoutes>
             },
            
             {
